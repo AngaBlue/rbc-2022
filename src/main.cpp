4ab002d3a5aa0a2b0d4fb010b57c1fecbd484dc5
AngaBlue <contact@anga.blue>
2022-08-27 14:16:32 +1000

    Speed control

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 // Number of colours to detect
 #define COLOUR_COUNT 5
+#define SPEED 50
 
 // Shared colour sensor pins
 #define S0 11
@@ -23,21 +24,21 @@
 #define U_OUT 3
 
 // Motor Control
-#define LEFT_ENA A0
-#define LEFT_IN1 A1
-#define LEFT_IN2 A2
+#define LEFT_ENA A3
+#define LEFT_IN1 A4
+#define LEFT_IN2 A5
 
-#define RIGHT_ENA A3
-#define RIGHT_IN1 A4
-#define RIGHT_IN2 A5
+#define RIGHT_ENA A0
+#define RIGHT_IN1 A1
+#define RIGHT_IN2 A2
 
 // Define RGB values for colours, these must match the same order as the enum
 int RGBColors[COLOUR_COUNT][3] = {
-    {6, 9, 10},     // Green
-    {35, 23, 14},   // Yellow
-    {25, 7, 9},     // Red
-    {5, 4, 5},      // Black
-    {52, 47, 60}    // White
+    {6, 9, 10},   // Green
+    {35, 23, 14}, // Yellow
+    {25, 7, 9},   // Red
+    {5, 4, 5},    // Black
+    {52, 47, 60}  // White
 };
 
 // Sensors
@@ -46,50 +47,50 @@ tcs3200 TCS_RIGHT(S0, S1, S2, S3, C_OUT_RIGHT);
 
 /**
  * @brief Moves the robot in the direction specified.
- * 
+ *
  * @param direction The direction to move the robot in.
  */
 void move(Direction direction)
 {
-    Serial.println("Moving " + directionNameFromEnum(direction));
+    // Serial.println("Moving " + directionNameFromEnum(direction));
 
     switch (direction)
     {
     case LEFT:
-        digitalWrite(LEFT_ENA, HIGH);
-        digitalWrite(RIGHT_ENA, HIGH);
+        analogWrite(LEFT_ENA, SPEED);
+        analogWrite(RIGHT_ENA, SPEED);
         digitalWrite(LEFT_IN1, LOW);
         digitalWrite(LEFT_IN2, HIGH);
         digitalWrite(RIGHT_IN1, HIGH);
         digitalWrite(RIGHT_IN2, LOW);
         break;
     case RIGHT:
-        digitalWrite(LEFT_ENA, HIGH);
-        digitalWrite(RIGHT_ENA, HIGH);
+        analogWrite(LEFT_ENA, SPEED);
+        analogWrite(RIGHT_ENA, SPEED);
         digitalWrite(LEFT_IN1, HIGH);
         digitalWrite(LEFT_IN2, LOW);
         digitalWrite(RIGHT_IN1, LOW);
         digitalWrite(RIGHT_IN2, HIGH);
         break;
     case FORWARD:
-        digitalWrite(LEFT_ENA, HIGH);
-        digitalWrite(RIGHT_ENA, HIGH);
+        analogWrite(LEFT_ENA, SPEED);
+        analogWrite(RIGHT_ENA, SPEED);
         digitalWrite(LEFT_IN1, HIGH);
         digitalWrite(LEFT_IN2, LOW);
         digitalWrite(RIGHT_IN1, HIGH);
         digitalWrite(RIGHT_IN2, LOW);
         break;
     case BACKWARD:
-        digitalWrite(LEFT_ENA, HIGH);
-        digitalWrite(RIGHT_ENA, HIGH);
+        analogWrite(LEFT_ENA, SPEED);
+        analogWrite(RIGHT_ENA, SPEED);
         digitalWrite(LEFT_IN1, LOW);
         digitalWrite(LEFT_IN2, HIGH);
         digitalWrite(RIGHT_IN1, LOW);
         digitalWrite(RIGHT_IN2, HIGH);
         break;
     case STOP:
-        digitalWrite(LEFT_ENA, LOW);
-        digitalWrite(RIGHT_ENA, LOW);
+        analogWrite(LEFT_ENA, SPEED);
+        analogWrite(RIGHT_ENA, SPEED);
         digitalWrite(LEFT_IN1, LOW);
         digitalWrite(LEFT_IN2, LOW);
         digitalWrite(RIGHT_IN1, LOW);
@@ -101,7 +102,7 @@ void move(Direction direction)
 void setup()
 {
     // Begin serial communication for logging
-    Serial.begin(9600);
+    // Serial.begin(9600);
 
     // Initialise all motor pins.  The colour sensors are initialised in the sensor constructor.
     pinMode(A0, OUTPUT);
@@ -119,7 +120,7 @@ void loop()
     Colour c_right = (Colour)TCS_RIGHT.closestColorIndex(RGBColors, COLOUR_COUNT);
 
     // Print the color values
-    Serial.println("Left: " + colourNameFromEnum(c_left) + " Right: " + colourNameFromEnum(c_right));
+    // Serial.println("Left: " + colourNameFromEnum(c_left) + " Right: " + colourNameFromEnum(c_right));
 
     // Find direction
     Direction direction = FORWARD;
